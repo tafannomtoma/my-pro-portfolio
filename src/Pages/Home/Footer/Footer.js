ec0bb94c9b001b44d708ade03f6b528aa679d1d0
tafannomtoma <tafannomtoma@gmail.com>
2021-11-30 15:43:07 +0600

    update-commit

--- src/Pages/Home/Footer/Footer.js
+++ src/Pages/Home/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
             <p style={{ color: 'white' }}>©copyright 2021 || Tafannom Ara Toma</p>
             <div>
                 <a href="https://github.com/tafannomtoma">GitHub</a>
-                {/* <a href="https://www.w3schools.com">Visit W3Schools.com!</a> */}
+                <a href="https://www.w3schools.com">Visit W3Schools.com!</a>
             </div>
 
         </div>
